28b45fd3d55e87b01bf3d1bbbd925607c0e042d1
yporret <yporret@micropole.com>
2017-11-18 19:28:00 +0100

    nav

--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,11 @@
-// Copyright 2016 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 
 (function() {
   'use strict';
 
   var app = {
     isLoading: true,
-    visibleCards: {},
-    selectedCities: [],
     spinner: document.querySelector('.loader'),
-    cardTemplate: document.querySelector('.cardTemplate'),
-    container: document.querySelector('.main'),
-    daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    container: document.querySelector('.main')
   };
 
 
@@ -33,16 +15,26 @@
    *
    ****************************************************************************/
 
-  document.getElementById('butRefresh').addEventListener('click', function(){
-    app.showMe('#home');
+  document.getElementById('butAchiv').addEventListener('click', function(){
+    app.showMe('#achiv');
   });
 
   document.querySelector('.temp').addEventListener('click', function(){
-    console.log('coucou');
     app.showMe('#defiDetail');
   });
 
   document.getElementById('retourDetail').addEventListener('click', function(){
+    app.showMe('#home');
+  });
+
+  document.getElementById('btnAddChallenge').addEventListener('click', function(){
+    app.showMe('#defiCreate');
+  });
+
+  document.getElementById('butProfil').addEventListener('click', function(){
+    app.showMe('#profil');
+  });
+  document.getElementById('butHome').addEventListener('click', function(){
     app.showMe('#home');
   });
   /*****************************************************************************
@@ -56,15 +48,10 @@
      container.removeClass('showMe').addClass('hideMe');
    }
    app.showMe = function(id){
-     // Refresh all of the forecasts
      var toDisplay = $(id);
      if(toDisplay[0].classList.contains('hideMe')){
        app.resetAllPage();
        toDisplay.removeClass('hideMe').addClass('showMe');;
-       //cardTemplate.setAttribute('hidden', true);
-     }else{
-       app.resetAllPage();
-       //cardTemplate.removeAttribute('hidden');
      }
    }
 
